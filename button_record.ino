1fcf3f58591e706fc0b50c473826380ac1c297a0
Alex Crosby <Alex.J.Crosby19@gmail.com>
2026-04-13 15:14:01 +0100

    Updated button_record.ino for on board microphone capture

--- button_record.ino
+++ button_record.ino
@@ -1,26 +1,69 @@
 const int LED_PIN = D2;
 const int BUTTON_PIN = D1;
 
-int lastButtonState = HIGH;
+int lastStableButtonState = HIGH;
+int lastReading = HIGH;
+
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 30;
+
+// Recording state
+bool isRecording = false;
+unsigned long recordingStartTime = 0;
+const unsigned long RECORDING_DURATION_MS = 3000;  
+
+void startRecording() {
+  isRecording = true;
+  recordingStartTime = millis();
+  digitalWrite(LED_PIN, HIGH);
+
+  Serial.println("RECORDING_START");
+
+}
+
+void stopRecording() {
+  isRecording = false;
+  digitalWrite(LED_PIN, LOW);
+
+  Serial.println("RECORDING_STOP");
+
+  Serial.println("AUDIO_READY_TO_SEND");
+}
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  digitalWrite(LED_PIN, LOW);
+
   Serial.begin(115200);
+  delay(500);
+  Serial.println("DEVICE_READY");
 }
 
 void loop() {
-  int currentButtonState = digitalRead(BUTTON_PIN);
+  int reading = digitalRead(BUTTON_PIN);
 
-  if (lastButtonState == HIGH && currentButtonState == LOW) {
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("BUTTON");
+  // Debounce logic
+  if (reading != lastReading) {
+    lastDebounceTime = millis();
   }
 
-  if (currentButtonState == HIGH) {
-    digitalWrite(LED_PIN, LOW);
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != lastStableButtonState) {
+      lastStableButtonState = reading;
+
+      // Button pressed
+      if (lastStableButtonState == LOW && !isRecording) {
+        startRecording();
+      }
+    }
   }
 
-  lastButtonState = currentButtonState;
-  delay(20);
+  lastReading = reading;
+
+  // Stop recording after fixed duration
+  if (isRecording && (millis() - recordingStartTime >= RECORDING_DURATION_MS)) {
+    stopRecording();
+  }
 }
